20d6b99e967ecd64a0dff639dd22aa30b096c8f1
h.stasiuk <h.stasiuk@zoovu.com>
2020-07-08 15:52:33 +0200

    Project init

---
+++ src/main/java/com/recipely/RecipelyApplication.java
@@ -0,0 +1,12 @@
+package com.recipely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipelyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/recipely/RecipelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipelyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
